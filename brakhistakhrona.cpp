f92ce81289bb2013c895ab9d7a3de81f83dcb23f
student <student@comp07.class218>
2017-04-14 15:01:05 +0400

    razbienie

--- brakhistakhrona.cpp
+++ brakhistakhrona.cpp
@@ -76,7 +76,7 @@ void fcn(double x, double *y, double *f) {
 
 //Метод Рунге-Кутты 5-ого порядка с автоматическим выбором шага
 #include "diploma_func/ddopri5_brakhistakhrona.cpp"
-#include "prak3_func/gauss_m.cpp"
+#include "diploma_func/gauss_m.cpp"
 
 void l(double *beta, double *res, double *y) {
 	double pvx2, pvy2, RHO, H;
@@ -112,79 +112,7 @@ void l(double *beta, double *res, double *y) {
 	
 }
 
-void gradf(double *beta, double *dbeta, double *res, double *y) {
-	int i, j, N=5;
-	double beta_p[N], beta_m[N], res_p[N], res_m[N], h=1.e-6;
-	double **pro, **pro2;
-
-	pro=(double**)malloc(N*sizeof(double*));
-	pro2=(double**)malloc(N*sizeof(double*));
-	for(i=0; i<N; i++) {
-		pro[i]=(double*)malloc(N*sizeof(double));
-		pro2[i]=(double*)malloc(N*sizeof(double));
-	}
-
-	for(j=0; j<N; j++) {
-		for(i=0; i<N; i++) {
-			beta_p[i]=beta[i];
-			beta_m[i]=beta[i];
-		}
-		
-		beta_p[j]+=h;
-		beta_m[j]-=h;
-		
-		l(beta_p, res_p, y);
-		l(beta_m, res_m, y);
-		
-		for(i=0; i<N; i++) pro[i][j]=(res_p[i]-res_m[i])/(2*h);
-	}
-	
-	matrix_print(pro, N);
-	
-	for(i=0; i<N; i++) res_m[i]=-res[i];
-	linear_sys(pro, dbeta, res_m, N);
-	
-	
-	//printf("TEST!");
-	
-	//matrix_print(pro2, N);
-}
-
-int NEWTON(double *beta, double *y) {
-	int i, j, N=5;
-	double res[N], res_w[N], beta_w[N], dbeta[N], gamma;
-	bool flag;
-	
-	l(beta, res, y);
-	
-	for(j=0; j<15; j++) {
-		cout << "\n-------\nj = " << j << "\n-------\n";
-	
-		if(fabs(res[0])<ens && fabs(res[1])<ens && fabs(res[2])<ens && fabs(res[3])<ens) {cout << endl <<"Ended by " << j << " iteration" << endl; return j;}
-		gradf(beta,dbeta,res,y);
-	
-		gamma=1.0;
-		flag=false;
-		while(gamma>eps_gamma) {
-			for(i=0; i<N; i++) 
-				{
-					beta_w[i]=beta[i]+gamma*dbeta[i];
-					//printf("beta_w: %.16f\n",beta_w[i]);
-				}
-			l(beta_w, res_w, y);
-			
-			if(norm(res_w,N)<norm(res,N)) {flag=true; break;}
-			gamma/=2.0;
-		}
-		if(flag==false) {cout << endl << "Broken on " << j << "'s iteration" << endl; return -1;}
-		for(i=0; i<N; i++) {
-			beta[i]=beta_w[i];
-			res[i]=res_w[i];
-		}
-	}
-cout << endl << "not enough iteration" << endl;
-return -2;
-}
+#include "diploma_func/newtoon.cpp"
 
 int main() {
 	double beta[5] = {1, 1, 1, 1, 1};
